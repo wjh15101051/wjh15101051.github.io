dc0d2a7291f90506edd9d534aa8e5bfacef5b38b
wjh15101051 <wjh15101051@163.com>
2020-10-29 16:35:46 +0800

    re 1.0

---
+++ jscode/frame.js
@@ -0,0 +1,40 @@
+function changetonight()
+{
+  var ModeChangeButtonImage = document.getElementById("mode_change_button_image");
+  ModeChangeButtonImage.src = "http://pic.yupoo.com/wjh15101051/e2bfab14/eafa1163.png";
+  var FrameStyle = document.getElementById("frame_style");
+  FrameStyle.href = "css/frame_style2.css";
+  var imgc;
+  imgc = document.getElementById("tags_image");
+  imgc.src = "icon/icon_style2/tags.png";
+  imgc = document.getElementById("categories_image");
+  imgc.src = "icon/icon_style2/categories.png";
+  imgc = document.getElementById("timeline_image");
+  imgc.src = "icon/icon_style2/timeline.png";
+  imgc = document.getElementById("about_image");
+  imgc.src = "icon/icon_style2/about.png";
+  imgc = document.getElementById("toolbox_image");
+  imgc.src = "icon/icon_style2/toolbox.png";
+  imgc = document.getElementById("friends_image");
+  imgc.src = "icon/icon_style2/friends.png";
+}
+function changetodaytime()
+{
+  var ModeChangeButtonImage = document.getElementById("mode_change_button_image");
+  ModeChangeButtonImage.src = "http://pic.yupoo.com/wjh15101051/3fe277ec/4db885c0.png";
+  var FrameStyle = document.getElementById("frame_style");
+  FrameStyle.href = "css/frame_style1.css";
+  var imgc;
+  imgc = document.getElementById("tags_image");
+  imgc.src = "icon/icon_style1/tags.png";
+  imgc = document.getElementById("categories_image");
+  imgc.src = "icon/icon_style1/categories.png";
+  imgc = document.getElementById("timeline_image");
+  imgc.src = "icon/icon_style1/timeline.png";
+  imgc = document.getElementById("about_image");
+  imgc.src = "icon/icon_style1/about.png";
+  imgc = document.getElementById("toolbox_image");
+  imgc.src = "icon/icon_style1/toolbox.png";
+  imgc = document.getElementById("friends_image");
+  imgc.src = "icon/icon_style1/friends.png";
+}
